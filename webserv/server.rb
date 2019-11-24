9965fadc370f8495977a9a3841cdc534485abc82
vagne0rlove9 <55533946+vagne0rlove9@users.noreply.github.com>
2019-11-25 00:07:19 +0300

    try to fix ct

--- webserv/server.rb
+++ webserv/server.rb
@@ -2,11 +2,22 @@
 require 'socket'
 
 server = TCPServer.new(9999)
-SERVER_ROOT='/home/user1/Документы/html/Counter/webserv'
+c=0
+c1="GET / HTTP/1.1"
+if(c1==="GET / HTTP/1.1\n")
+  puts c
+end
+SERVER_ROOT='C:\Users\Кирилл\Documents\GitHub\Counter\ulab\index.html'
 while (session = server.accept)
   request = session.gets
+  c=c+1
+
+  if (request.to_s.eql?(c1))
+    puts request
+  end
+  puts request
   method, full_path = request.split(' ')
-  file_path="#{SERVER_ROOT}#{full_path}"
+  file_path="#{SERVER_ROOT}"
   response = "HTTP/1.1 200\r\n"
   response << "Content-Type : text/html\r\n"
   response << "\r\n"
